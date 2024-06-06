222b6f9a008203b921605bf4b7e5402f7512d503
cverab <cverab@outlook.com>
2024-06-05 19:10:44 -0500

    Webpage Correcciones

--- src/App.js
+++ src/App.js
@@ -10,9 +10,9 @@ function App() {
     <div className="App">
       <Header autor='Carlos Vera' oficio='Desarrollador Web' />
       <About />
-      <Seccion title='Proyectos Realizados' jsonUrl="http://localhost:3005/proyectos/" color="#FFFFFF" back="#FFB923" />
-      <Seccion title='Formación Academica' jsonUrl="http://localhost:3005/formacion/" color="#000000" back="#FFFFFF" />
-      <Seccion title='Mis Intereses' jsonUrl="http://localhost:3005/intereses/" color="#FFFFFF" back="#FFB923" />
+      <Seccion title='Proyectos Realizados' jsonUrl="https://json-server-vercel-steel-sigma.vercel.app/proyectos/" color="#FFFFFF" back="#FFB923" />
+      <Seccion title='Formación Academica' jsonUrl="https://json-server-vercel-steel-sigma.vercel.app/formacion/" color="#000000" back="#FFFFFF" />
+      <Seccion title='Mis Intereses' jsonUrl="https://json-server-vercel-steel-sigma.vercel.app/intereses/" color="#FFFFFF" back="#FFB923" />
       <Contact />
       <Footer autor='Carlos Vera' back="#FFB923" />
     </div>
